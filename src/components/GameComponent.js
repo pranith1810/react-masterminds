c2f5bb78030f87681bcd5ac739f4f5ba4c83fe85
pranith9810 <n.rao@mountblue.tech>
2020-09-07 19:22:45 +0530

    Added the logic for matching the input number with the generated random number for the GameComponent and logging the result

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -3,11 +3,55 @@ import '../styles/GameComponent.css';
 
 class GameComponent extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            
+            difficulty: '',
+            numberInput: 0,
+            randomNumber: 0
         };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleDifficultyChange = this.handleDifficultyChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    generateRandomNumber() {
+        let randNum = 0;
+        console.log(this.state.difficulty);
+        if (this.state.difficulty === 'easy') {
+            randNum = Math.floor(Math.random() * 10);
+        }
+        else if (this.state.difficulty === 'medium') {
+            randNum = Math.floor(Math.random() * 100);
+        }
+        else {
+            randNum = Math.floor(Math.random() * 1000);
+        }
+        this.setState({ randomNumber: randNum });
+    }
+
+    handleDifficultyChange(event) {
+        this.setState({
+            difficulty: event.target.id
+        }, this.generateRandomNumber);
+        
+    }
+
+    handleInputChange(event) {
+        this.setState({ numberInput: event.target.value });
+    }
+
+    handleClick() {
+        if (this.state.difficulty === '') {
+            alert('Please select the difficulty');
+        }
+        if (Number(this.state.numberInput) === this.state.randomNumber) {
+            console.log('Number matched');
+        }
+        else {
+            console.log("Try again");
+        }
+        console.log(this.state);
     }
 
     render() {
@@ -15,20 +59,20 @@ class GameComponent extends React.Component {
             <div className='game'>
                 <div className='radio-buttons'>
                     <label>
-                        <input type='radio' name='difficulty' value='Easy' />
+                        <input onChange={this.handleDifficultyChange} type='radio' name='difficulty' id='easy' checked={this.state.difficulty === 'easy'} />
                         Easy
                     </label>
                     <label>
-                        <input type='radio' name='difficulty' value='Medium' />
+                        <input onChange={this.handleDifficultyChange} type='radio' name='difficulty' id='medium' checked={this.state.difficulty === 'medium'} />
                         Medium
                     </label>
                     <label>
-                        <input type='radio' name='difficulty' value='Hard' />
+                        <input onChange={this.handleDifficultyChange} type='radio' name='difficulty' id='hard' checked={this.state.difficulty === 'hard'} />
                         Hard
                     </label>
                 </div>
-                <input className='number-input' type = 'number' />
-                <button className='guess'>Guess</button>
+                <input onChange={this.handleInputChange} value={this.state.numberInput} className='number-input' type='number' />
+                <button onClick={this.handleClick} className='guess'>Guess</button>
             </div>
         );
     }
