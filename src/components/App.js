7c6e1f39829d48b42b27306701a190918e7e0245
pranith9810 <n.rao@mountblue.tech>
2020-09-09 11:39:55 +0530

    Added a react router in app.js file with routes to '/' and '/about' paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from './Header.js';
 import GameComponent from './GameComponent.js';
+import About from './About.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className = 'whole-container'>
+    <div className='whole-container'>
       <Header />
-      <GameComponent />
+      <Router>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <GameComponent />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
